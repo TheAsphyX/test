0712f78200634202808d43fffce43efeb20af848
luigi.frasca@vigilfuoco.it <Luigi>
2017-03-28 11:37:30 +0200

    Aggiunto formcontrol e submit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FormChiamataComponent } from './form-chiamata/form-chiamata.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormChiamataComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
